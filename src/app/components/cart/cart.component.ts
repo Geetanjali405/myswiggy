f81f47ed00c6b93c4e04eb3685681b3a5911f680
Geetanjali <geetanjali.bgs@gmail.com>
2023-11-15 13:02:27 +0530

    displayed card details

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { UserService } from 'src/shared/services/user.service';
@@ -6,10 +6,9 @@ import { UserService } from 'src/shared/services/user.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
- 
   subscription: Subscription;
   cart: any;
   userId: string;
@@ -21,13 +20,31 @@ export class CartComponent implements OnInit {
 
   ngOnInit(): void {
     this.userId = localStorage.getItem('id');
-    this.userService.getCart(this.userId).subscribe(cart => {
-      this.cart = cart; 
-      console.log("line 26");
-      console.warn(cart);
-    }, error => {
-      console.error('Error fetching cart: ', error);
-    });
-
+    this.userService.getCart(this.userId).subscribe(
+      (cart) => {
+        this.cart = cart;
+        console.log('line 26');
+        console.warn(cart);
+      },
+      (error) => {
+        console.error('Error fetching cart: ', error);
+      }
+    );
+  }
+  removeItem(arg0: unknown) {
+    throw new Error('Method not implemented.');
+  }
+  increaseItem(menuId: any) {
+    this.userService.addItemToCart(this.userId, menuId).subscribe(
+      (data) => {
+        console.log('Item increased by 1 successfully!');
+      },
+      (error) => {
+        console.error('Error while increasing item in cart: ' + error);
+      }
+    );
+  }
+  decreaseItem(arg0: unknown) {
+    throw new Error('Method not implemented.');
   }
 }
