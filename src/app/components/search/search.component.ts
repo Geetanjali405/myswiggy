8a45fdcac7fe17c47a423a073ff0fd0f6c85a675
Geetanjali <geetanjali.bgs@gmail.com>
2023-11-18 17:14:13 +0530

    designed search and profile

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -15,6 +15,60 @@ export class SearchComponent {
   subscription: Subscription;
   searchForm: FormGroup;
 
+  public images = [
+    {
+      image:
+        'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/b4ff78ecc5b8b66f732dd06228916d65',
+    },
+    {
+      image:
+        'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/3df4fca020027e89b89c733cdffc4966',
+    },
+    {
+      image:
+        'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/5dd234f7decdac4b4f71a2ff1408e10f',
+    },
+    {
+      image:
+        'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/e76b511935016406e6ebc11dd7593387',
+    },
+    {
+      image:
+        'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/87664acb0f9dd95d10a549bb8190ab27',
+    },
+    {
+      image:
+        'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/89f3fec702aef5acbb51a6cbc284b3f7',
+    },
+    {
+      image:
+        'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/8322f6d6df488dc1f5a6674cfe863f0f',
+    },
+    {
+      image:
+        'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/31f03222ea978aef3b10d386729eb076',
+    },
+    {
+      image:
+        'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/31f03222ea978aef3b10d386729eb076',
+    },
+    {
+      image:
+        'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/c170aa4262ec0d191642f42a3a03b4ce',
+    },
+    {
+      image:
+        'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/0b5ffa32a04d99c1f212d2aacefd5f6f',
+    },
+    {
+      image:
+        'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/36184033ebef97d27a85fa3af5c1d403',
+    },
+    {
+      image:
+        'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/rng/md/carousel/production/d0884e09ef431ee610e54a0bb2dfecd5',
+    },
+  ];
   constructor(
     private router: Router,
     private userService: UserService,
@@ -44,8 +98,7 @@ export class SearchComponent {
     console.log(query);
     if (query.length === 0) {
       this.filteredProducts = [];
-    }
-    else if (query.length > 1) {
+    } else if (query.length > 1) {
       this.filteredProducts = this.menuList.filter((item) => {
         return (
           item.name.toLowerCase().includes(query.toLowerCase()) ||
